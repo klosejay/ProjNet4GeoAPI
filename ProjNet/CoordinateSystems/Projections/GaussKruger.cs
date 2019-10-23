3ae18fdade0641985278713a694ba043dbf16233
klose <klose.jay@gmail.com>
2019-10-23 21:55:38 +0800

    Add "Gauss_Kruger" projection surport.
    the projection is very common in china.

---
+++ ProjNet/CoordinateSystems/Projections/GaussKruger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjNet.CoordinateSystems.Projections
+{
+    internal class GaussKruger:TransverseMercator
+    {
+        public GaussKruger(IEnumerable<ProjectionParameter> parameters) : base(parameters)
+        {
+            Name = "Gauss_Kruger";
+        }
+    }
+}
